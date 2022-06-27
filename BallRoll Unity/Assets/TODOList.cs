f95d8d467d16bb3b0e34b124ff1e46c715fb7503
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2022-06-26 21:33:38 -0500

    Added Standard Assets

---
+++ BallRoll Unity/Assets/TODOList.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TODOList
+{
+    //Level Win!
+    //Collectibles with Sound and UI
+    //Moving Platforms
+    //Levels for everyone in class
+}
