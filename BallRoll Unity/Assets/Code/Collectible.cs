153f3f48304cc5ab506ccd3a5b1bc07ab3c4b8dd
Matt Burton <matthewwburton@gmail.com>
2022-08-23 12:45:08 -0500

    Removed stuffs

--- BallRoll Unity/Assets/Code/Collectible.cs
+++ BallRoll Unity/Assets/Code/Collectible.cs
@@ -5,10 +5,5 @@ using DG.Tweening;
 
 public class Collectible : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        SoundManager.Instance.PlayCollectSound();
-        CollectibleManager.Instance.IncrementCollectedCoinCount();
-        Destroy(gameObject);
-    }
+
 }
